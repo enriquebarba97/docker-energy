a4a60dd514c4e0a462113b1712d317a25ca54135
Bailey Tjiong <btjiong@outlook.com>
2023-04-01 00:54:04 +0200

    Adding initial Dockerfile and monitor command for llama.cpp

---
+++ llama.cpp/monitor.sh
@@ -0,0 +1,1 @@
+perf stat -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ docker run -v /home/bailey/Documents/llama/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "The capital of The Netherlands:" -n 20
