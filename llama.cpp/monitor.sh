987f7de2d3d0c42c578d29b463d9b48f218a8935
Bailey Tjiong <btjiong@outlook.com>
2023-04-05 13:46:36 +0200

    Update Dockerfiles and monitoring script for llama.cpp

--- llama.cpp/monitor.sh
+++ llama.cpp/monitor.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-perf stat -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ docker run -v /home/bailey/Documents/llama/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "The capital of The Netherlands:" -n 20
+
+# Get the arguments
+while getopts "b:n:" arg; do
+  case $arg in
+    b) BASE=$OPTARG;;
+    n) NUMBER=$OPTARG;;
+  esac
+done
+
+# Default values
+BASE=${BASE:-ubuntu}
+NUMBER=${NUMBER:-1}
+
+# If alpine is selected, use the alpine Dockerfile
+if [[ "$BASE" == "alpine" ]]; then 
+  DOCKERFILE=DockerfileAlpine; 
+else 
+  DOCKERFILE=Dockerfile; 
+fi
+
+# Build the Docker image
+docker build -t llama.cpp -f $DOCKERFILE --build-arg BASE=$BASE .
+
+# Run and monitor the workload for the specified number of times
+for ((i=1; i<=$NUMBER; i++))
+do
+  perf stat -o result.txt --append -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ docker run --rm -v $PWD/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "The capital of The Netherlands:" -n 20
+done
