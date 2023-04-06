3fd24ed04021bb68071c63bc750df263f5302525
Bailey Tjiong <btjiong@outlook.com>
2023-04-06 12:09:29 +0200

    Add an argument for the inference input

--- llama.cpp/monitor.sh
+++ llama.cpp/monitor.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Get the arguments
-while getopts "b:n:" arg; do
+while getopts "b:n:i:" arg; do
   case $arg in
     b) BASE=$OPTARG;;
     n) NUMBER=$OPTARG;;
+    i) INFERENCE=$OPTARG;;
   esac
 done
 
 # Default values
 BASE=${BASE:-ubuntu}
 NUMBER=${NUMBER:-1}
+INFERENCE=${INFERENCE:-"The capital of The Netherlands:"}
 
 # If alpine is selected, use the alpine Dockerfile;
 if [[ "$BASE" == "alpine" ]]; then 
@@ -32,7 +34,7 @@ sysbench cpu --cpu-max-prime=100000 --threads=2 run
 for ((i=1; i<=$NUMBER; i++))
 do
   perf stat -o result.txt --append -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ docker run --rm \
-  -v $PWD/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "The capital of The Netherlands:" -n 20 --seed 12345678
+  -v $PWD/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "$INFERENCE" -n 20 --seed 12345678
   sleep 10
 done
 
