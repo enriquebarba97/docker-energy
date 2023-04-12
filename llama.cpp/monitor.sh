60bc1fa522084eb2682a4dffc08ae5dca1385231
Bailey Tjiong <btjiong@outlook.com>
2023-04-12 14:00:51 +0200

    Small changes to monitoring script

--- llama.cpp/monitor.sh
+++ llama.cpp/monitor.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Get the arguments
-while getopts "b:n:i:" arg; do
+while getopts "b:n:i:t:" arg; do
   case $arg in
     b) BASE=$OPTARG;;
     n) NUMBER=$OPTARG;;
     i) INFERENCE=$OPTARG;;
+    t) TOKENS=$OPTARG;;
   esac
 done
 
 # Default values
 BASE=${BASE:-ubuntu}
 NUMBER=${NUMBER:-1}
-INFERENCE=${INFERENCE:-"The capital of The Netherlands:"}
+INFERENCE=${INFERENCE:-"Building a website can be done in 10 simple steps:"}
+TOKENS=${TOKENS:-512}
 
 # If alpine is selected, use the alpine Dockerfile;
 if [[ "$BASE" == "alpine" ]]; then 
@@ -34,8 +36,8 @@ sysbench cpu --cpu-max-prime=100000 --threads=2 run
 for ((i=1; i<=$NUMBER; i++))
 do
   sleep 10
-  perf stat -o result.txt --append -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ docker run --rm \
-  -v $PWD/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "$INFERENCE" -n 20 --seed 12345678
+  perf stat -o result.txt --append -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ sleep 180 &\
+  docker run --rm -v $PWD/models:/models llama.cpp -m /models/7B/ggml-model-q4_0.bin -p "$INFERENCE" -n $TOKENS --seed 12345678
 done
 
 docker rmi llama.cpp
