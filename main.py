ed1646c80e56e939ae67a8123bcac7f3e1c96b9f
Bailey Tjiong <btjiong@outlook.com>
2023-04-16 21:10:51 +0200

    Add option to build multiple images

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import sys, getopt, subprocess, uuid
 def build(argv):
     print(argv)
 
+def shuffle(argv):
+    print(argv)
+
+def run(command):
+    subprocess.call(command)
+
 def main(argv):
     runid = str(uuid.uuid4())
     command = ['bash', 'monitor', '-u', runid]
     # base = ""
-    opts, args = getopt.getopt(argv, "b:n:i:t:")
+    opts, args = getopt.getopt(argv, "b:n:i:t:a")
     for opt, arg in opts:
         command += [opt, arg]
-    subprocess.call(command)
+    run(command)
+    # print(command)
 
 if __name__ == '__main__':
     # build(sys.argv[1:])
