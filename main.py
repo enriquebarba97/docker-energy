f694b93fef4847ed6bf3c2d8fe8fbf1a204bcb00
Bailey Tjiong <btjiong@outlook.com>
2023-04-16 20:34:25 +0200

    Restructure scripts into prepare phase and monitoring phase, and update logs

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import sys, getopt, subprocess
+import sys, getopt, subprocess, uuid
+
+def build(argv):
+    print(argv)
 
 def main(argv):
-    command = ['bash', 'monitor']
+    runid = str(uuid.uuid4())
+    command = ['bash', 'monitor', '-u', runid]
     # base = ""
     opts, args = getopt.getopt(argv, "b:n:i:t:")
     for opt, arg in opts:
         command += [opt, arg]
-        # base += " " + opt + " " + arg
-        # if opt == "-b":
-        #     base = arg
     subprocess.call(command)
 
 if __name__ == '__main__':
+    # build(sys.argv[1:])
     main(sys.argv[1:])
