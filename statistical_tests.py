4af0bb998cdeb75dfee590274baa75ab17ff0505
Bailey Tjiong <btjiong@outlook.com>
2023-05-01 16:33:17 +0200

    Clean up statistical_tests script

--- statistical_tests.py
+++ statistical_tests.py
@@ -7,16 +7,26 @@ from scipy import stats
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 
+def get_lists(data):
+    # Prepare the lists for the results
+    # Labels are the base images
+    labels = list(data.keys())
+    # Parts are the different measurements (e.g. CPU, GPU, etc.)
+    parts = list(data[labels[0]].columns)
+    result = list()
+    return labels, parts, result
+
+
 def shapiro_test(data):
     print("False means that the null hypothesis is rejected, i.e. the data is not normal.")
-    labels = list(data.keys())
-    parts = list(data[labels[0]].columns)
-    normal = list()
+    labels, parts, normal = get_lists(data)
+    # Perform the Shapiro-Wilk test for normality for each part of each base image
     for l in labels:
         n = list()
         for p in parts:
             x = data[l][p].values.astype(float)
             shapiro = stats.shapiro(x)
+            # If the p-value is less than 0.05, the null hypothesis is rejected (i.e. the data is not normal)
             if shapiro.pvalue < 0.05:
                 n.append(False)
             else:
@@ -28,15 +38,15 @@ def shapiro_test(data):
 
 def anova_test(data):
     print("False means that the null hypothesis is rejected, i.e. the means are not equal.")
-    labels = list(data.keys())
-    parts = list(data[labels[0]].columns)
-    significance = list()
+    labels, parts, significance = get_lists(data)
+    # Perform the one-way ANOVA test for each part between the base images
     for p in parts:
         x = list()
         s = True
         for l in labels:
             x.append(data[l][p].values.astype(float))
         anova = stats.f_oneway(*x)
+        # If the p-value is less than 0.05, the null hypothesis is rejected (i.e. the means are not equal)
         if anova.pvalue < 0.05:
             significance.append(False)
             s = False
@@ -48,11 +58,9 @@ def anova_test(data):
 
 
 def tukey_test(data):
-    labels = list(data.keys())
-    parts = list(data[labels[0]].columns)
-    tukey = list()
+    labels, parts, tukey = get_lists(data)
+    # Perform the Tukey HSD test for each part between the base images
     for p in parts:
-        # for key, value in data.items():
         scores = list()
         for l in labels:
             scores.extend(data[l][p].values.astype(float))
@@ -69,17 +77,18 @@ def tukey_test(data):
 
 def read_tsv(file):
     with open(file) as f:
-        # read the first line for the base image name
+        # Read the first line for the base image name
         base = f.readline().rstrip().split("\t")[0]
-        # read the second line for the column names
+        # Read the second line for the column names
         second_line = f.readline().rstrip().split("\t")
     return base, pd.read_csv(file, sep="\t", skiprows=2, names=second_line)
 
 
-def main(argv):
-    images = {}
+def parse_args(argv):
     file = list()
     statistical_test = ""
+
+    # Get the arguments provided by the user
     opts, args = getopt.getopt(argv, "f:", ["file=", "shapiro", "anova", "tukey"])
     for opt, arg in opts:
         if opt in ["-f", "--file"]:
@@ -91,6 +100,14 @@ def main(argv):
         if opt == "--tukey":
             statistical_test = "tukey"
 
+    return file, statistical_test
+
+
+def main(argv):
+    images = {}
+    file, statistical_test = parse_args(argv)
+
+    # Read the TSV files
     for f in file:
         base, df = read_tsv(f)
         images[base] = df
