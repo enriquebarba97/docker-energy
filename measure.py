f6db4696f76adb72a545d5eb97a424ab8dae9b52
Bailey Tjiong <btjiong@outlook.com>
2023-06-05 18:05:21 +0200

    Clean up the measure pipeline

--- measure.py
+++ measure.py
@@ -25,16 +25,15 @@ def help():
     print(
         "A tool for measuring energy consumption for specific workloads using different base images.\n",
         "Options:",
-        "   -l              Workload to monitor (e.g. -l \"llama.cpp\" or -l \"video-stream\")",
-        "   -b              Base image to monitor; can be used for multiple base images (e.g. -b ubuntu -b alpine) (default \"ubuntu\")",
-        "   -n              Number of monitoring runs per base image (e.g. -n 30) (default 1)",
-        "   -w              Warm up time (s) (default 10)",
-        "   -p              Pause time (s) (default 10)",
-        "   -i              Change the inference input for llama.cpp (default \"Building a website can be done in 10 simple steps:\")",
-        "   -o              Options for the Docker run command (default \"\")",
-        "   -c              Command for the Docker run command (default \"\")",
-        "   -a              Monitor all compatible base images (i.e. ubuntu, debian, alpine, centos)",
-        "   --shuffle       Enables shuffle mode; random order of monitoring base images",
+        "   -l --workload       Workload to monitor (e.g. -l \"llama.cpp\" or -l \"video-stream\")",
+        "   -b --base           Base image to monitor; can be used for multiple base images (e.g. -b ubuntu -b alpine) (default \"ubuntu\")",
+        "   -n --runs           Number of monitoring runs per base image (e.g. -n 30) (default 1)",
+        "   -w --warmup         Warm up time (s) (e.g. -w 30) (default 10)",
+        "   -p --pause          Pause time (s) (e.g. -p 60) (default 15)",
+        "   -o --options        Options for the Docker run command (default \"\")",
+        "   -c --command        Command for the Docker run command (default \"\")",
+        "   -a --all            Monitor all compatible base images (i.e. ubuntu, debian, alpine, centos)",
+        "   --shuffle           Enables shuffle mode; random order of monitoring base images",
         sep=os.linesep
     )
 
@@ -56,47 +55,68 @@ def parse_args(argv):
     help_mode = False
 
     # Get the arguments provided by the user
-    opts, args = getopt.getopt(argv, "l:b:n:w:p:ui:o:c:ah", ["shuffle", "help"])
+    opts, args = getopt.getopt(argv, "l:"  # workload
+                                     "b:"  # base image
+                                     "n:"  # number of runs
+                                     "w:"  # warm up time
+                                     "p:"  # pause time
+                                     "o:"  # options for the Docker run command
+                                     "c:"  # command for the Docker run command
+                                     "a"   # all compatible base images
+                                     "h",  # help
+                               ["shuffle",
+                                "help",
+                                "all",
+                                "workload=",
+                                "base=",
+                                "runs=",
+                                "warmup=",
+                                "pause=",
+                                "options=",
+                                "command="])
     for opt, arg in opts:
         # Set shuffle mode to true
         if opt == "--shuffle":
             shuffle_mode = True
         # Add the images to the list and the preparation command
-        elif opt == "-l":
+        elif opt in ["-l", "--workload"]:
             workload = arg
+            opt = "-l"
             prepare_command += [opt, arg]
             monitor_command += [opt, arg]
         # Add the images to the list and the preparation command
-        elif opt == "-b":
+        elif opt in ["-b", "--base"]:
             images.add(arg)
         # Set the number of runs
-        elif opt == "-n":
+        elif opt in ["-n", "--runs"]:
             number = arg
         # Set up the warm up time (s)
-        elif opt == "-w":
+        elif opt in ["-w", "--warmup"]:
+            opt = "-w"
             prepare_command += [opt, arg]
         # Set up the pause time (s)
-        elif opt == "-p":
+        elif opt in ["-p", "--pause"]:
+            opt = "-p"
             monitor_command += [opt, arg]
-        elif opt == "-u":
-            monitor_command += [opt]
-        # Add the additional arguments to the monitoring command
-        # -i is the inference input for llama.cpp;
-        # -o are the options for the Docker run command;
-        # -c is the command for the Docker run command;
-        elif opt in ["-i", "-o", "-c"]:
+        # Set the options for the Docker run command
+        elif opt in ["-o", "--options"]:
+            opt = "-o"
+            monitor_command += [opt, arg]
+        # Set the command for the Docker run command
+        elif opt in ["-c", "--command"]:
+            opt = "-c"
             monitor_command += [opt, arg]
         # Set help mode to true
         elif opt in ["-h", "--help"]:
             help_mode = True
         # Add all (pre-selected) images to the image set
-        elif opt == "-a":
+        elif opt in ["-a", "--all"]:
             images |= {"ubuntu", "alpine", "debian"}
 
     # Add the images that needs to be built to the prepare command
     for image in images:
         prepare_command += ["-b", image]
-        queue += [image]*int(number)
+        queue += [image] * int(number)
 
     # The queue is a dictionary with the image as key and number of runs as value
     # queue = {x: int(number) for x in images}
