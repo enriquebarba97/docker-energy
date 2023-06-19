fa8a56f8434383292b04f424f6457fe271b88fc8
Bailey Tjiong <btjiong@outlook.com>
2023-06-19 21:34:48 +0200

    Add cpu pinning for docker container and warmup (provisional

--- measure.py
+++ measure.py
@@ -1,4 +1,4 @@
-import os, sys, getopt, subprocess, uuid, random
+import os, sys, getopt, subprocess, uuid, random, re
 import parse
 
 
@@ -49,10 +49,10 @@ def parse_args(argv):
     # Default values
     workload = ""
     images = set()
-    queue = list()
     number = 1
     shuffle_mode = False
     help_mode = False
+    cpus = ""
 
     # Get the arguments provided by the user
     opts, args = getopt.getopt(
@@ -65,8 +65,11 @@ def parse_args(argv):
         "o:"  # options for the Docker run command
         "c:"  # command for the Docker run command
         "a"  # all compatible base images
+        "i:"  # isolate cpus
+        "s"  # shuffle mode
         "h",  # help
         [
+            "isolate=",
             "shuffle",
             "help",
             "all",
@@ -81,9 +84,11 @@ def parse_args(argv):
     )
     for opt, arg in opts:
         # Set shuffle mode to true
-        if opt == "--shuffle":
+        if opt in ["-s", "--shuffle"]:
             shuffle_mode = True
         # Add the images to the list and the preparation command
+        elif opt in ["-i", "--isolate"]:
+            cpus = arg
         elif opt in ["-l", "--workload"]:
             workload = arg
             opt = "-l"
@@ -97,7 +102,9 @@ def parse_args(argv):
                 arg[-1] == ":"
             ):  # If the version is specified but empty, use the latest
                 arg += "latest"
-            images.add(arg)
+            if arg not in images:
+                images.add(arg)
+                prepare_command += ["-b", arg]
         # Set the number of runs
         elif opt in ["-n", "--runs"]:
             number = arg
@@ -126,27 +133,59 @@ def parse_args(argv):
         elif opt in ["-a", "--all"]:
             images |= {"ubuntu", "alpine", "debian", "centos"}
 
-    # Add the images that needs to be built to the prepare command
-    for image in images:
-        prepare_command += ["-b", image]
-        queue += [image] * int(number)
+    # # Add the images that needs to be built to the prepare command
+    # for image in images:
+    #     prepare_command += ["-b", image]
+    #     queue += [image] * int(number)
 
-    # The queue is a dictionary with the image as key and number of runs as value
-    # queue = {x: int(number) for x in images}
-    if shuffle_mode:
-        random.shuffle(queue)
+    # # The queue is a dictionary with the image as key and number of runs as value
+    # # queue = {x: int(number) for x in images}
+    # if shuffle_mode:
+    #     random.shuffle(queue)
 
     # Put the arguments in a dictionary
     arguments = {
         "prepare_command": prepare_command,
         "monitor_command": monitor_command,
-        "queue": queue,
+        "images": images,
+        "number": number,
         "shuffle_mode": shuffle_mode,
         "help_mode": help_mode,
         "workload": workload,
         "exp_id": exp_id,
+        "cpus": cpus,
     }
     return arguments
+
+
+def init_queue(images, number, shuffle_mode):
+    queue = list()
+    for image in images:
+        queue += [image] * int(number)
+    if shuffle_mode:
+        random.shuffle(queue)
+    return queue
+
+
+def set_cpus(cpus):
+    isolate_cpus = ",".join(
+        str(i) for i in list(range(os.cpu_count() // 2, os.cpu_count()))
+    )
+    background_cpus = ",".join(str(i) for i in list(range(os.cpu_count() // 2)))
+
+    if cpus == "":
+        return isolate_cpus, background_cpus
+
+    if "-" in cpus:
+        print("Specify a set of CPUs instead of a range (e.g. -i 0,1,2,3)")
+
+    for c in re.split(",|-", cpus):
+        if c not in isolate_cpus:
+            print(f"CPU {c} is not available, select a core from {isolate_cpus}")
+
+    isolate_cpus = cpus
+
+    return isolate_cpus, background_cpus
 
 
 def main(argv):
@@ -159,18 +198,27 @@ def main(argv):
         return
 
     # If no workload is specified, do not monitor
-    if len(arguments["workload"]) == 0:
-        print('No workload provided (e.g. -w "llama.cpp" or -w "video-stream")')
-        return
+    # if len(arguments["workload"]) == 0:
+    #     print('No workload provided (e.g. -w "llama.cpp" or -w "video-stream")')
+    #     return
 
-    if len(arguments["queue"]) == 0:
+    if len(arguments["images"]) == 0:
         print("No base images provided (e.g. -b ubuntu -b alpine)")
         return
+
+    isolate_cpus, background_cpus = set_cpus(arguments["cpus"])
+
+    arguments["prepare_command"] += ["-i", isolate_cpus, "-j", background_cpus]
+    arguments["monitor_command"] += ["-i", isolate_cpus, "-j", background_cpus]
+
+    queue = init_queue(
+        arguments["images"], arguments["number"], arguments["shuffle_mode"]
+    )
 
     # Initiate the preparation phase: building the images and warming up the machine
     execute(arguments["prepare_command"])
 
-    run(arguments["monitor_command"], arguments["queue"])
+    run(arguments["monitor_command"], queue)
 
     # Remove the base images used in the experiment
     remove_command = arguments["prepare_command"].copy()
