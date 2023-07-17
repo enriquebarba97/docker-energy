3f7187e8ee90586dfb7c6df8aa0cfabdb39f86c4
Bailey Tjiong <btjiong@outlook.com>
2023-07-17 15:23:43 +0200

    Provisional ubuntu image with pgbench; still need to fix problems with alpine image and running pgbench in another container

---
+++ workloads/postgres-server/docker-entry.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# PGPASSWORD=postgres
+POSTGRES_PASSWORD=postgres
+POSTGRES_PASSWORD_USER=postgres
+
+# service postgresql start
+initdb -D /var/lib/postgresql/data
+postgres -D /var/lib/postgresql/data
+
+createdb testdb
+
+pgbench -i -s 50 testdb
+
+pgbench -c 1 -j 2 -T 10 testdb
