61d8c6988964db255037163de3e84e25c55af5d8
Bailey Tjiong <btjiong@outlook.com>
2023-07-20 13:24:38 +0200

    Fixes and small changes to postgres-server

--- workloads/postgres-server/docker-entry.sh
+++ workloads/postgres-server/docker-entry.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [[ "$DIST" == "centos" ]]; then
-  initdb -D /var/lib/pgsql/14/data
-  pg_ctl -D /var/lib/pgsql/14/data start &
+  initdb -D /var/lib/pgsql/${VERSION}/data
+  pg_ctl -D /var/lib/pgsql/${VERSION}/data start &
 else
-  initdb -D /var/lib/postgresql/14/main
-  postgres -D /var/lib/postgresql/14/main -c config_file=/etc/postgresql/14/main/postgresql.conf &
+  initdb -D /var/lib/postgresql/${VERSION}/main
+  postgres -D /var/lib/postgresql/${VERSION}/main -c config_file=/etc/postgresql/${VERSION}/main/postgresql.conf &
 fi
 
 sleep 5
